e17fb02379a2ab3ba7b17283a0f8023093d251d7
duseongKing <108321764+duseongKing@users.noreply.github.com>
2022-06-30 16:53:43 +0900

    시환

--- camily/src/main/java/com/camily/controller/BoardController.java
+++ camily/src/main/java/com/camily/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.camily.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BoardController {
@@ -11,5 +13,16 @@ public class BoardController {
 		System.out.println("게시판 이동 요청");
 		
 		return "board/BoardList";
+	}
+	
+	@RequestMapping(value="/boardDetail")
+	public ModelAndView boardDetail() {
+		System.out.println("게시판 상세글 조회");
+		
+		ModelAndView mav = new ModelAndView();
+		
+		
+		
+		return mav;
 	}
 }
